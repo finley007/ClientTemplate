51b1ef35054bb3c5052842603e54c668b8ac079a
Li Liu <finley@lis-macbook-pro.cn.ibm.com>
2017-06-05 16:34:09 +0800

    Create framework

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    var handlebars = require('handlebars');
+
+    var config = {
+        // configurable paths
+        src: 'src/web-template',
+        dev: 'dev/web-template',
+        dist: 'dist/web-template'
+    };
+
+    grunt.initConfig({
+        config: config,
+        serve: {
+                options: {
+                    port: 8080,
+                    serve: {
+                        path: './dev/web-template/'
+                    }
+                }
+            },
+
+        watch: {
+            options: {
+                // these two options significantly decrease execution time
+                spawn: false,
+                interrupt: true
+            },
+
+            deploy: {
+                files: ['<%= config.src %>/**/*'],
+                tasks: ['copy:dev']
+            }
+        },
+
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= config.src %>',
+                    dest: '<%= config.dev %>',
+                    src: [
+                        './**/*'
+                    ]
+                }]
+            },
+
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= config.src %>',
+                    dest: '<%= config.dist %>',
+                    src: [
+                        './**/*'
+                    ]
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('dev', [
+        'copy:dev',
+        'watch'
+    ]);
+
+    grunt.registerTask('dist', [
+        
+    ]);
+
+    grunt.registerTask('default', ['dev']);
+};
