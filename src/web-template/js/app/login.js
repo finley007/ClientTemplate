d590574b1bd1741d5e39c0fdb16a43b989a86483
Li Liu <finley@lis-macbook-pro.cn.ibm.com>
2017-11-30 17:34:29 +0800

    登录Demo

--- src/web-template/js/app/login.js
+++ src/web-template/js/app/login.js
@@ -2,17 +2,47 @@
 // 登录的相关逻辑在该模块中实现
 define(function(require) {
 
-	var $ = require('jquery');
+    var $ = require('jquery');
 
     var loginModule = {
-    	//初始化界面
+        //初始化界面
         initPage: function() {
             console.log("Init login page");
-            // $("body").append("aa");
-        }
+            $('#submit').click(function(event) {
+                if (!loginModule.login()) {
+                    event.preventDefault();
+                }
+            });
+
+        },
 
         //监听事件
-        
+        //submit监听
+        login: function() {
+            var uname = $('#username').val(),
+                pwd = $('#password').val();
+            if (uname === "") {
+                loginModule.invalidUsernameHint();
+                return false;
+            }
+            if (pwd === "") {
+                loginModule.invalidUserHint();
+                return false;
+            }
+            return true;
+        },
+
+
+        //错误提示
+        //用户名
+        invalidUsernameHint: function() {
+            alert("用户名不能为空");
+        },
+
+        //密码
+        invalidUserHint: function() {
+            alert("密码不能为空");
+        }
     };
 
     return loginModule;
