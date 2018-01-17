924239353577f3871e9727c36d389982cb2df43d
Li Liu <finley@Lis-MacBook-Pro.local>
2018-01-17 14:53:38 +0800

    为登录界面绑定处理脚本

---
+++ src/web-template/js/login.js
@@ -0,0 +1,45 @@
+var app = app || {};
+/**
+ * mailCtrl
+ * @namespace urx.mailCtrl
+ */
+app.login = app.login || {};
+(function($) {
+    app.login = {
+        
+        emailRegx : new RegExp(/email/),
+        pwdRegx : new RegExp(/pwd/),
+
+        init:function() {
+            $('#login-form').submit(app.login.login);
+            $('#username').bind('input', function() {
+                $('#username-error').text('').addClass('hide');
+            });
+            $('#password').bind('input', function() {
+                $('#pwd-error').text('').addClass('hide');
+            });
+        },
+
+        login:function() {
+            var username = $('#username').val();
+            var password = $('#password').val();
+            if (username === '') {
+                $('#username-error').removeClass('hide').text('请输入邮箱');
+                return false;
+            } else if (!app.login.emailRegx.test(username)) {
+                 $('#username-error').removeClass('hide').text('邮箱格式不正确');
+                 return false;
+            }
+            if (password === '') {
+                $('#pwd-error').removeClass('hide').text('请输入密码');
+                return false;
+            } else if (!app.login.pwdRegx.test(password)) {
+                $('#pwd-error').removeClass('hide').text('密码格式不合要求');
+                return false;
+            }
+        }
+        
+    }
+
+    app.login.init();
+})(jQuery);
