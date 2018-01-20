d740f614dd00fa6494b234abbc5a44d90c7d580e
Li Liu <finley@Lis-MacBook-Pro.local>
2018-01-20 15:32:26 +0800

    重构项目结构

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,8 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
 
     grunt.loadNpmTasks('grunt-connect-apimock');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     var handlebars = require('handlebars');
 
@@ -13,29 +15,52 @@ module.exports = function(grunt) {
         // configurable paths
         src: 'src/web-template',
         dev: 'dev/web-template',
-        dist: 'dist/web-template'
+        dist: 'dist/web-template',
+        lib: 'lib'
     };
 
     grunt.initConfig({
+        config: config,
+
+        concat: {
+            options: {
+                stripBanners: true,
+                banner: '/*! Comment for app */'
+            },
+            dev: {
+                src: ['<%= config.src %>/**/*.js'],
+                dest: 'dev/web-template/js/app.js'
+            }
+        },
+
+        uglify: {
+            dev: {
+                options: {
+                    mangle: false, //混淆变量名
+                    preserveComments: 'false' //不删除注释，还可以为 false（删除全部注释），some（保留@preserve @license @cc_on等注释）
+                },
+                src: 'dev/web-template/js/app.js',
+                dest: 'dev/web-template/js/app.min.js'
+            }
+        },
+
         connect: {
-          options: {
+            options: {
                 port: 8000,
                 hostname: 'localhost',
-                middleware: function (connect) {
+                middleware: function(connect) {
                     var middlewares = [];
                     //add the apimock middleware to connect 
                     middlewares.push(require('grunt-connect-apimock/lib/apimock').mockRequest);
                     return middlewares;
                 }
-          },
-          apimock: {
-            //apimock configuration 
-            url: '/myApp/api/',
-            dir: 'mock'
-          }
+            },
+            apimock: {
+                //apimock configuration 
+                url: '/myApp/api/',
+                dir: 'mock'
+            }
         },
-
-        config: config,
 
         serve: {
             options: {
@@ -60,6 +85,17 @@ module.exports = function(grunt) {
         },
 
         copy: {
+            lib: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= config.lib %>/js',
+                    dest: '<%= config.dev %>/js/lib',
+                    src: [
+                        './**/*'
+                    ]
+                }]
+            },
             dev: {
                 files: [{
                     expand: true,
@@ -67,19 +103,9 @@ module.exports = function(grunt) {
                     cwd: '<%= config.src %>',
                     dest: '<%= config.dev %>',
                     src: [
-                        './**/*'
-                    ]
-                }]
-            },
-
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= config.src %>',
-                    dest: '<%= config.dist %>',
-                    src: [
-                        './**/*'
+                        './**/*.html',
+                        './**/*.css',
+                        './**/*.jpg'
                     ]
                 }]
             }
@@ -88,9 +114,17 @@ module.exports = function(grunt) {
         clean: ['dev', 'dest']
     });
 
+    grunt.registerTask('mock', [
+        'configureApimock',
+        'connect',
+        'watch'
+    ]);
+
     grunt.registerTask('dev', [
         'clean',
-        'copy:dev',
+        'concat',
+        'uglify',
+        'copy',
         'serve'
     ]);
 
@@ -100,5 +134,4 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', ['dev']);
 
-    grunt.registerTask('mock', ['configureApimock', 'connect', 'watch']);
 };
